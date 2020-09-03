92a9f6c5ffcaf45516474339df2f53f94b17c8fc
vishnu.g <VishnuGurudathan>
2020-09-03 22:56:40 +0530

    removing unused imports

--- logging-spring-boot-autoconfigure/src/main/java/com/vishnu/boot/logging/autoconfigure/LoggingAutoconfigure.java
+++ logging-spring-boot-autoconfigure/src/main/java/com/vishnu/boot/logging/autoconfigure/LoggingAutoconfigure.java
@@ -1,7 +1,6 @@
 package com.vishnu.boot.logging.autoconfigure;
 
 import com.vishnu.service.aop.LoggableAspect;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
